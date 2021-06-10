0136af5f13d447116785e7e9794ec792c6caf5bd
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-10 17:02:20 +0200

    different-ranges

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
+// function randomNum(min, max) {
+
+// }
 computerNum = [];
 userNum = [];
 maxNum = 16;
 userMaxNum = 100 - maxNum;
+min = 1;
+max = 0;
 
-function randomNum() {
-    return Math.floor(Math.random() * 100) + 1;
+var difficulty = prompt('Che livello di difficoltà scegli? 0, 1 o 2?');
+while (difficulty !== "0" && difficulty !== "1" && difficulty !== "2") {
+    alert('Devi inserire un livello di difficiltà tra 0, 1 e 2!');
+    var difficulty = prompt('Che livello di difficoltà scegli? 0, 1 o 2?');
+}
+
+switch (difficulty) {
+    case "0":
+        max += 100;
+        break;
+    case "1":
+        max += 80;
+        break;
+    case "2":
+        max += 50;
+        break;
 }
 
 while (computerNum.length < maxNum) {
-    var element = randomNum();
+    var element = Math.floor(Math.random() * (max - min + 1)) + min;
     if (!computerNum.includes(element)) {
         computerNum.push(element);
     }
@@ -16,7 +35,7 @@ while (computerNum.length < maxNum) {
 
 console.log(computerNum);
 
-while (userNum.length < userMaxNum && !computerNum.includes(element2) && !userNum.includes(element2)) {
+while (userNum.length < userMaxNum && !computerNum.includes(element2)) {
     var element2 = parseInt(prompt('Inserisci un numero!'));
     if (computerNum.includes(element2)) {
         alert('Ops! Hai trovato una mina!');
@@ -29,5 +48,7 @@ while (userNum.length < userMaxNum && !computerNum.includes(element2) && !userNu
 }
 
 document.getElementById('score').innerHTML = 'Hai totalizzato un punteggio di ' + userNum.length;
+
+// -------------------------------------------------
 
 
