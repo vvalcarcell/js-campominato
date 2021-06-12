6b495fcbea9a3f2a2187332f2d10025be441449c
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-12 12:44:58 +0200

    bonus-maybe

---
+++ bonus/script.js
@@ -0,0 +1,49 @@
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var hundredList = [];
+var i = 0;
+var button = document.getElementById('button');
+var classCell = document.getElementsByClassName('cell');
+var computerList = [2, 3, 4]
+
+
+button.addEventListener("click", function () {
+    while (hundredList.length < 10) {
+
+        var num = getRandomNum(1, 10);
+        if (!hundredList.includes(num)) {
+            hundredList.push(num);
+
+            classCell[i].innerHTML = num;
+            // document.getElementsByClassName("cell")[i].setAttribute("value", num);
+            i += 1;
+        }
+
+    }
+
+    for (var x = 0; x < 10; x++) {
+        classCell[x].onclick = function () {
+            if (computerList.includes(parseInt(this.innerHTML))) {
+                this.style.backgroundColor = "red";
+            } else {
+                this.style.backgroundColor = "green";
+
+            }
+        }
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
